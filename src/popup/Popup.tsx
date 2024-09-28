1730b5f7e60339cafbe4cfe8c410b9377445fd41
Sijan Mainali <sijanmainali2@gmail.com>
2024-09-28 17:24:39 -0500

    added login with google button using shadcn ui

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,3 +1,15 @@
+import { Button, buttonVariants } from '../components/ui/button'
+import { cn } from '../lib/utils'
+
 export default function MyExtension() {
-  return <h1>Hello, Extension!</h1>
+  return (
+    <div className='h-40 w-80'>
+      <Button
+        type='button'
+        className={cn(buttonVariants({ variant: 'outline' }))}
+      >
+        Login with Google
+      </Button>
+    </div>
+  )
 }
